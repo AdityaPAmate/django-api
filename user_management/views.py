531b87bd1b48f0b754ea09e06ce2ab1d724477a9
Aditya Amate <adityaamate84@gmail.com>
2026-06-02 13:46:46 +0530

    first commit

---
+++ user_management/views.py
@@ -0,0 +1,81 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
+
+
+
+#create signup api
+@csrf_exempt
+def signup(request):
+    #check the method is POST or not
+    if request.method == 'POST':
+
+        #convert the json to dictionary and store in one variable
+        data= json.loads(request.body)
+
+        #fetch the values of username and password and save in seperate variables
+        username= data.get('username')
+        password= data.get('password')
+
+        #store the data into user table which inbuilt in sqlite database
+        User.objects.create_user(
+            username = username,
+            password = password
+        )
+
+        #return response as User Created successfully
+        return JsonResponse({
+            'message' : 'User Created Successfully'
+        })
+
+    return JsonResponse({
+        'message': 'Only POST Request is Allowed'
+    })
+
+#create login api
+from django.contrib.auth import authenticate
+@csrf_exempt
+def login_view(request):
+    #check if request is post or not
+    if request.method == 'POST':
+
+        #convert the json into dictionary and store in variable
+        data= json.loads(request.body)
+
+        #extract and store the username and password values in different variable
+        username = data.get('username')
+        password = data.get('password')
+
+        #authenticate it and return User Object otherwise None
+        user = authenticate(
+            username=username,
+            password=password
+        )
+
+        #check if user contain User Object or not
+        if user :
+            return  JsonResponse ({
+                'message' : 'Login Successfully'
+            })
+        else:
+            return  JsonResponse({
+                'message' : 'Invalid Username or Password'
+            })
+
+    return  JsonResponse({
+        'message': 'Only POST Request is Allowed'
+    })
+
+
+
+
+
+
+
+
+
+
